f7d33bdf405405248abbfa3866b922a80ddd3606
Mayur Chaudhari <mayursunilchaudhari@gmail.com>
2026-08-31 14:12:39 +0530

    Strengthen MQTT client manager tests

--- test/client-manager-test.js
+++ test/client-manager-test.js
@@ -7,11 +7,7 @@ const manager = new EdgeHubMqttClientManager();
 
 assert.equal(manager.users, 0);
 assert.equal(manager.isConnected(), false);
-assert.equal(manager.inputListeners.size, 0);
-assert.equal(manager.methodHandlers.size, 0);
-assert.equal(manager.twinDesiredListeners.size, 0);
 
-let releaseCalled = false;
 const removeInput = manager.addInputListener('input1', () => {});
 const removeTwin = manager.addTwinDesiredListener(() => {});
 const removeMethod = manager.addMethodHandler('method1', () => {});
@@ -20,18 +16,31 @@ assert.equal(manager.inputListeners.size, 1);
 assert.equal(manager.twinDesiredListeners.size, 1);
 assert.equal(manager.methodHandlers.size, 1);
 
+assert.throws(
+    () => manager.addMethodHandler('method1', () => {}),
+    /already registered/
+);
+
 removeInput();
 removeTwin();
 removeMethod();
-releaseCalled = manager.inputListeners.size === 0
-    && manager.twinDesiredListeners.size === 0
-    && manager.methodHandlers.size === 0;
-assert.equal(releaseCalled, true);
 
+assert.equal(manager.inputListeners.size, 0);
+assert.equal(manager.twinDesiredListeners.size, 0);
+assert.equal(manager.methodHandlers.size, 0);
+
+let connectingEvents = 0;
+let connectedEvents = 0;
+let disconnectedEvents = 0;
+manager.on('connecting', () => connectingEvents++);
+manager.on('connected', () => connectedEvents++);
+manager.on('disconnected', () => disconnectedEvents++);
 manager.emit('connecting');
 manager.emit('connected');
 manager.emit('disconnected');
 
-assert.equal(manager.client, null);
+assert.equal(connectingEvents, 1);
+assert.equal(connectedEvents, 1);
+assert.equal(disconnectedEvents, 1);
 
 console.log('MQTT client manager lifecycle primitives passed.');
